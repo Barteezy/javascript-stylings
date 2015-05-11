4dd7b75bb98f6e2f8eb948fa4cd9e607d60733fc
Luke Bartel <bartelluke@gmail.com>
2015-05-10 20:24:13 -0600

    html styled with javascript

---
+++ app.js
@@ -0,0 +1,41 @@
+var mainNav = document.getElementsByClassName("mainnav");
+mainNav[0].style.backgroundColor = "#1f2126";
+mainNav[0].style.color = "#d6d6d6";
+mainNav[0].style.width = "%100";
+mainNav[0].style.height = "50px";
+mainNav[0].style.marginTop = "-20px";
+
+var body = document.getElementsByTagName("body");
+body[0].style.margin = "0px";
+
+var h1 = document.getElementsByTagName("h1");
+h1[0].style.textAlign = "center";
+h1[0].style.paddingTop = "10px";
+h1[0].style.fontFamily = "arial";
+
+var square = document.getElementsByClassName("square");
+square[0].style.height = "200px";
+square[0].style.width = "33.3%";
+square[0].style.backgroundColor = "#64c7c7";
+square[0].style.float = "left";
+
+square[1].style.height = "200px";
+square[1].style.width = "33.3%";
+square[1].style.backgroundColor = "#3a3e82";
+square[1].style.float = "left";
+
+square[2].style.height = "200px";
+square[2].style.width = "33.3%";
+square[2].style.backgroundColor = "#1d1a49";
+square[2].style.float = "left";
+
+var footer = document.getElementsByClassName("footer");
+footer[0].style.backgroundColor = "#d6d6d6";
+footer[0].style.color = "#1f2126";
+footer[0].style.padding = "2px"
+
+var h3 = document.getElementsByTagName("h3");
+h3[0].style.textAlign = "center";
+h3[0].style.fontFamily = "arial";
+h3[0].style.marginBottom = "10px";
+h3[0].style.marginTop = "10px";
